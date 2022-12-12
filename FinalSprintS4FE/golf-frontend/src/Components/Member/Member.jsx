f412c1a110712edbb7e835c7e5f29bcfe374bde2
WesleySquire <Wesley.Squire@keyin.com>
2022-12-12 17:08:34 -0330

    Update Members Page

--- FinalSprintS4FE/golf-frontend/src/Components/Member/Member.jsx
+++ FinalSprintS4FE/golf-frontend/src/Components/Member/Member.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Fragment } from 'react';
+import MembersHeader from '../Layout/MembersHeader';
+import Block from '../../UI/Block';
+import Button from '../../UI/Button';
+import classes from '../../UI/Button.module.css';
+import { IoIosAddCircle } from 'react-icons/io';
+import { MdViewList } from 'react-icons/md';
 
 const Member = () => {
-  return (
-    <div>
-      <h1>Member</h1>
-      <h1>By Wesley Squire!!!!</h1>
+  const iconAdd = <IoIosAddCircle />;
+  const iconView = <MdViewList />;
+  const buttonGroup = (
+    <div className={classes.btnContainer}>
+      <Button label='Add Member' style={classes} icon={iconAdd} />
+      <Button label='View Members ' style={classes} icon={iconView} />
     </div>
+  );
+
+  return (
+    <Fragment>
+      <Block>
+        <MembersHeader title='Members' button={buttonGroup} />
+      </Block>
+    </Fragment>
   );
 };
 
