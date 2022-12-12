2af6c5b04653c67cf605f168aee981d06e1193ab
GeorgeRuby <96242129+GeorgeRuby@users.noreply.github.com>
2022-12-12 10:01:51 -0330

    intial commit

--- FinalSprintS4FE/golf-frontend/src/api/services/members/getMembers.js
+++ FinalSprintS4FE/golf-frontend/src/api/services/members/getMembers.js
@@ -1,10 +1,11 @@
-import { API_ENDPOINT } from "../../connection/server";
+import { API_ENDPOINT } from '../../connection/server'
 
 const getMembers = async () => {
-  const res = await fetch(API_ENDPOINT + "/api/member/all");
-  const data = await res.json();
+  const res = await fetch(API_ENDPOINT + '/api/member/all')
+  const data = await res.json()
 
-  return data;
-};
+  return data
+}
 
-export { getMembers };
+export { getMembers }
+// George's change
