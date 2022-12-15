28ecf77cc923ad93cdb5720c650ccbdbe8d81839
Tarsolan <aridgeley@msn.com>
2022-12-15 14:46:07 -0330

    adding members to tournament work

--- FinalSprintS4FE/golf-frontend/src/Components/Tournament/TournamentDetails/AddMemberToTournament.jsx
+++ FinalSprintS4FE/golf-frontend/src/Components/Tournament/TournamentDetails/AddMemberToTournament.jsx
@@ -1,38 +1,69 @@
-import React, { useState } from 'react'
-import Button from '../../../UI/Button'
-import Select from '../../../UI/Select'
-import btnCss from '../../../UI/Button.module.css'
+import React, { useContext, useState } from "react";
+import Button from "../../../UI/Button";
+import Select from "../../../UI/Select";
+import btnCss from "../../../UI/Button.module.css";
 import { MdPeople } from "react-icons/md";
-import Card from '../../../UI/Card';
-
+import Card from "../../../UI/Card";
+import MemberContext from "../../Context/member-context";
+import TournamentContext from "../../Context/tournament-context";
 
 const AddMemberToTournament = () => {
-    const [membersToAdd, setMembersToAdd] = useState([]);
-    const [memberCount, setMemberCount] = useState(0);
-    const [memberArray, setMemberArray] = useState([<Select setMembersToAdd={setMembersToAdd} memberToAdd = {membersToAdd} />]);
-    
-    const iconPeople = <MdPeople />;
-    
-    const onAddMemberClick = () =>{
-        let count = memberCount + 1;
-        setMemberCount(count);
-        setMemberArray([...memberArray, <Select setMembersToAdd={setMembersToAdd} memberToAdd = {membersToAdd}/>])
-    }
+  const memCtx = useContext(MemberContext);
 
-    
+  const filterByReference = (arr1, arr2) => {
+    let res = [];
+    res = arr1.filter((el) => {
+      return !arr2.find((element) => {
+        return element.member.id === el.id;
+      });
+    });
+    return res;
+  };
+
+  const tourCtx = useContext(TournamentContext);
+  const members = filterByReference(
+    memCtx.members,
+    tourCtx.currentTournament.members
+  );
+  const iconPeople = <MdPeople />;
+
+  const [membersToAdd, setMembersToAdd] = useState([]);
+  const [memberArray, setMemberArray] = useState([
+    <Select
+      setMembersToAdd={setMembersToAdd}
+      membersToAdd={membersToAdd}
+      members={members}
+    />,
+  ]);
+
+  const onAddMemberClick = () => {
+    setMemberArray([
+      ...memberArray,
+      <Select
+        setMembersToAdd={setMembersToAdd}
+        membersToAdd={membersToAdd}
+        members={members}
+      />,
+    ]);
+  };
+
+  const submitMembers = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Card>
-    <div >
-        <Button 
-         label="Add Another Member"
-         style={btnCss}
-         icon={iconPeople}
-         handleClick={onAddMemberClick}
+      <div>
+        <Button
+          label="Add Another Member"
+          style={btnCss}
+          icon={iconPeople}
+          handleClick={onAddMemberClick}
         />
-        {memberArray}      
-    </div>
+        <form onSubmit={submitMembers}>{memberArray}</form>
+      </div>
     </Card>
-  )
-}
+  );
+};
 
-export default AddMemberToTournament
+export default AddMemberToTournament;
--- FinalSprintS4FE/golf-frontend/src/UI/Select.jsx
+++ FinalSprintS4FE/golf-frontend/src/UI/Select.jsx
@@ -1,48 +1,55 @@
-import Form from 'react-bootstrap/Form';
-import {useContext} from 'react';
-import MemberContext from '../Components/Context/member-context';
-import { useState } from 'react';
-import InputState from './InputState';
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import InputState from "./InputState";
 import classes from "./Select.module.css";
-import Card from './Card';
-function Select({setMembersToAdd, membersToAdd}) {
+import Button from "./Button";
+import { IoIosAddCircle } from "react-icons/io";
 
-    const memCtx = useContext(MemberContext);
-    const members = memCtx.members
-    const [memberObject, setMemberObject] = useState({});
-    const [memberId, setMemberId] = useState("");
-    const [score, setScore] = useState(0);
-    const onSelect=() =>{
-        setMembersToAdd([...membersToAdd, {
-            id: memberId,
-            score: score,
-        }])
-    }
+function Select({ members, setMembersToAdd, membersToAdd }) {
+  const [memberId, setMemberId] = useState("");
+  const [score, setScore] = useState(0);
 
-    
+  const handleChange = (id) => {
+    setMembersToAdd([...membersToAdd, { id: memberId, score: score }]);
+  };
+
   return (
-    <div className={classes.selectContainer} >
-    <Form.Select aria-label="Member Select">
-      <option className={classes.option}>Open this select menu to add a member</option>
-      {members.map((m) =>{
-        return(<option onChange={(e)=>setMemberId(e.target.value)} id= {m.id} value={m.id}>{m.firstName} {m.lastName}</option>) 
-      })}
-    </Form.Select>
-    <InputState 
-    formControl=''
-    label= {null}
-    input={{
-      id: 'score',
-      type: 'number',
-      min: '-30',
-      max: '30',
-      placeholder: 'score',
-      defaultValue: score,
-    }}
-    onChange={setScore}
-     />
+    <div className={classes.selectContainer}>
+      <Form.Select
+        aria-label="Member Select"
+        onChange={(e) => setMemberId(e.target.value)}
+      >
+        <option className={classes.option} disabled>
+          Open this select menu to add a member
+        </option>
+        {members.map((m) => {
+          return (
+            <option id={m.id} value={m.id}>
+              {m.firstName} {m.lastName}
+            </option>
+          );
+        })}
+      </Form.Select>
+      <InputState
+        formControl=""
+        label={null}
+        input={{
+          id: "score",
+          type: "number",
+          min: "-30",
+          max: "30",
+          placeholder: "score",
+          defaultValue: score,
+        }}
+        onChange={setScore}
+      />
+      <Button
+        label={null}
+        style={classes}
+        handleClick={handleChange}
+        icon={<IoIosAddCircle />}
+      />
     </div>
-    
   );
 }
 
