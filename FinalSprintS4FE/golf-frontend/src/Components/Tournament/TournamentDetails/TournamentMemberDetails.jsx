2ea569b17e5ca7eccf85b072ee88e73cff1eacd7
Mike Wadden <mike.wadden@keyin.com>
2022-12-15 06:50:51 -0330

    small change

--- FinalSprintS4FE/golf-frontend/src/Components/Tournament/TournamentDetails/TournamentMemberDetails.jsx
+++ FinalSprintS4FE/golf-frontend/src/Components/Tournament/TournamentDetails/TournamentMemberDetails.jsx
@@ -6,17 +6,18 @@ import Card from '../../../UI/Card'
 const TournamentMemberDetails = ({styles}) => {
     const tourCtx = useContext(TournamentContext)
     const members = tourCtx.currentTournament.members;
-    // {for(let i = 0; i < members.length; i++){
-    //     console.log(members[i].member.firstName)
-        
-    // }} 
+
+    const scoreCheck =(score) =>{
+       return score ? (score) : "N/A"
+    }
+
     const memberRow = (
 
             <tbody>
             { members.map((m,i)=>(
                 <tr>
-                <td key ={m.member.id}>{`${m.member.firstName} ${m.member.lastName}`}</td>  
-                <td key ={i}>{m.score}</td>
+                <td key ={m.member.id}>{`${m.member.firstName} ${m.member.lastName}`}</td>    
+                <td key ={i}>{scoreCheck(m.score)}</td>
               
                 </tr>           
             ))}
