f087c9ab5415b46f43456be042983163e64d64cf
Tarsolan <aridgeley@msn.com>
2022-12-16 12:55:05 -0330

    beginning of toast, small bug fixes

--- FinalSprintS4FE/golf-frontend/src/App.jsx
+++ FinalSprintS4FE/golf-frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css';
-import Footer from './Components/Main/Footer';
-import NavBar from './Components/Main/Nav';
-import Member from './Components/Member/Member';
-import Tournament from './Components/Tournament/Tournament';
-import Main from './Components/Main/Main';
-import NotFound from './Components/Main/notFound';
-import { Fragment, useEffect, useState, useContext } from 'react';
-import Block from './UI/Block';
-import { Routes, Route } from 'react-router-dom';
-import { getData } from './api/services/getData';
-import TournamentContext from './Components/Context/tournament-context';
-import TournamentRoutes from './Components/Tournament/TournamentRoutes';
-import MemberContext from './Components/Context/member-context';
-import MemberRoutes from './Components/Member/MemberRoutes';
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./Components/Main/Footer";
+import NavBar from "./Components/Main/Nav";
+import Member from "./Components/Member/Member";
+import Tournament from "./Components/Tournament/Tournament";
+import Main from "./Components/Main/Main";
+import NotFound from "./Components/Main/notFound";
+import { Fragment, useEffect, useState, useContext } from "react";
+import Block from "./UI/Block";
+import { Routes, Route } from "react-router-dom";
+import { getData } from "./api/services/getData";
+import TournamentContext from "./Components/Context/tournament-context";
+import TournamentRoutes from "./Components/Tournament/TournamentRoutes";
+import MemberContext from "./Components/Context/member-context";
+import MemberRoutes from "./Components/Member/MemberRoutes";
+import { ToastContainer } from "./utils/hooks/useToast";
 
 function App() {
   const tourCtx = useContext(TournamentContext);
@@ -29,13 +31,14 @@ function App() {
         <NavBar />
       </header>
 
-      <main className='App'>
+      <main className="App">
+        <ToastContainer />
         <Block>
           <Routes>
-            <Route path='/' element={<Main />} />
-            <Route path='/members/*' element={<MemberRoutes />} />
-            <Route path='/tournaments/*' element={<TournamentRoutes />} />
-            <Route path='*' element={<NotFound />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/members/*" element={<MemberRoutes />} />
+            <Route path="/tournaments/*" element={<TournamentRoutes />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Block>
       </main>
