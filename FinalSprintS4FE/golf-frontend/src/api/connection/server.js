6bd942a683695f92a8b192714abc260b1632c860
Mike Wadden <mike.wadden@keyin.com>
2022-12-19 11:41:34 -0330

    server

--- FinalSprintS4FE/golf-frontend/src/api/connection/server.js
+++ FinalSprintS4FE/golf-frontend/src/api/connection/server.js
@@ -1,7 +1,3 @@
-
 const API_ENDPOINT = "http://localhost:8080";
 // const API_ENDPOINT = "http://semester4-sprint2.us-east-1.elasticbeanstalk.com";
-//const API_ENDPOINT = process.env.API_ENDPOINT;
-const API_ENDPOINT = "http://localhost:8080";
-//const API_ENDPOINT = "http://semester4-sprint2.us-east-1.elasticbeanstalk.com";
 export { API_ENDPOINT };
