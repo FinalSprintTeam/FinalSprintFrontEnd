2ef32e94037d3e22e45bd357baa8599733b3c0ef
Tarsolan <aridgeley@msn.com>
2022-12-19 10:55:00 -0330

    small updates

--- FinalSprintS4FE/golf-frontend/src/api/connection/server.js
+++ FinalSprintS4FE/golf-frontend/src/api/connection/server.js
@@ -1,3 +1,4 @@
-//const API_ENDPOINT = 'http://localhost:8080';
-const API_ENDPOINT = "http://semester4-sprint2.us-east-1.elasticbeanstalk.com";
+//const API_ENDPOINT = process.env.API_ENDPOINT;
+const API_ENDPOINT = "http://localhost:8080";
+//const API_ENDPOINT = "http://semester4-sprint2.us-east-1.elasticbeanstalk.com";
 export { API_ENDPOINT };
